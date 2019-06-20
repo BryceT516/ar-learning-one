a672728de006ed95ab6d090683912cdaed484de2
Bryce Tucker <bryce.tucker@gmail.com>
2019-06-20 03:38:40 +0000

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { AFrameRenderer, Marker } from 'react-web-ar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <AFrameRenderer
+        arTookKit={{ sourceType: 'webcam', sourceUrl: ''}}
+        stats
+      >
+        <Marker parameters={{ preset: 'hiro'}}>
+          <a-box color='pink' material='opacity: 1;' position="0 0,003 0" scale='0.4 0.4 0.4'>
+            <a-animation attribute="rotation" to="360 0 0" dur="5000" easing="linear" repeat="indefinite" />
+          </a-box>
+        </Marker>
+      </AFrameRenderer>
+      )
+  }
 }
 
 export default App;
